ac71dbb8774d5467704c5d425c86148ff160aed2
Kacper Madej <kacper@blacklabel.pl>
2018-08-27 11:07:26 +0200

    docs: Corrected internal docs.

--- tasks/release.js
+++ tasks/release.js
@@ -3,18 +3,18 @@
  * Copy CHANGELOG.md up into the main dir.
  */
 
-const child_process = require( 'child_process' ),
-      fs = require( 'fs' ),
-      path = require( 'path' );
+const fs = require('fs'),
+      path = require('path');
 
-const rootDir = path.normalize( __dirname + '/..' ),
+const rootDir = path.normalize(__dirname + '/..'),
       codeDir = `${rootDir}/highcharts-angular`;
 
 // Copy CHANGELOG.md to the main folder for GitHub
-console.log( 'Copying ./highcharts-angular/CHANGELOG.md to the main directory' );
-fs.copyFileSync( `${codeDir}/CHANGELOG.md`, `${rootDir}/CHANGELOG.md` );
+console.log('Copying ./highcharts-angular/CHANGELOG.md to the main directory');
+fs.copyFileSync(`${codeDir}/CHANGELOG.md`, `${rootDir}/CHANGELOG.md`);
 
 // Further instructions
-console.log( '-------------------------------------------------------' );
-console.log( 'Push as `git push --follow-tags origin master`' );
-console.log( 'Publish in `./dist/highcharts-angular` as `npm publish`' );
+console.log('-------------------------------------------------------');
+console.log('Add moved CHANGELOG.md and `git commit --amend` it');
+console.log('Push as `git push --follow-tags origin master`');
+console.log('Publish in `./dist/highcharts-angular` as `npm publish`');
