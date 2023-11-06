668faff9b4f96849278435a08a52eb06e7fad565
karolkolodziej <karolkolodziej01@gmail.com>
2023-11-06 08:31:43 +0100

    Updated to angular 16 and changed to full compilationMode.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./polyfills.ts";
+import "./polyfills";
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
