b52b80f0d8d7e5d54dae5f5f795d001b6bc8d77c
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-11-21 23:27:42 +0100

    remove polyfills.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import "./polyfills";
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
