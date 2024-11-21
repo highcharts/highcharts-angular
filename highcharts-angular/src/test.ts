ded9d70ce1a91a370e42b01f1d79ba2ddb580dbe
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-11-21 23:48:13 +0100

    fix tslint issue

--- highcharts-angular/src/test.ts
+++ highcharts-angular/src/test.ts
@@ -1,13 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import { getTestBed } from "@angular/core/testing";
+import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
-} from "@angular/platform-browser-dynamic/testing";
-import "core-js/es/reflect";
-import "zone.js";
-import "zone.js/testing";
+} from '@angular/platform-browser-dynamic/testing';
+import 'zone.js/testing';
 
 declare const require: any;
 
@@ -17,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context("./", true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
