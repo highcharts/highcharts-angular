de4aadb900b0acf855683d4644bfebc585c5e9ca
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-11-30 00:54:00 +0100

    correct test component

--- src/app/tests/line-test/line-test.component.spec.ts
+++ src/app/tests/line-test/line-test.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { provideHighCharts, HighchartsChartService } from '../../../../highcharts-angular/src/public_api';
 import { LineTestComponent } from './line-test.component';
 
 
 describe('LineTestComponent', () => {
   let component: LineTestComponent;
   let fixture: ComponentFixture<LineTestComponent>;
+  let service: HighchartsChartService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [LineTestComponent]
+      imports: [LineTestComponent],
+      providers: [provideHighCharts({})]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(LineTestComponent);
+    service = TestBed.inject(HighchartsChartService);
+    service.load();
     component = fixture.componentInstance;
     component.updateSeriesColor();
     fixture.detectChanges();
@@ -24,16 +29,22 @@ describe('LineTestComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have data given from chartOptions', () => {
+  it('should have data given from chartOptions', (done) => {
     const chartOptions = component.chartOptions;
-    const chart = component.Highcharts.charts[component.Highcharts.charts.length - 1];
-    const series = chart.series[0];
-    expect((series as any).yData).toEqual((chartOptions.series[0] as any).data);
+    service.loaderChanges$.subscribe(loader => {
+      const chart = loader.charts[loader.charts.length - 1];
+      const series = chart.series[0];
+      expect((series as any).yData).toEqual((chartOptions.series[0] as any).data);
+      done();
+    });
   });
 
-  it('should be properly updated', () => {
-    const chart = component.Highcharts.charts[component.Highcharts.charts.length - 1];
-    const series = chart.series[0] as unknown as Highcharts.Series;
-    expect(series.color).toEqual('hotpink');
+  it('should be properly updated', (done) => {
+    service.loaderChanges$.subscribe(loader => {
+      const chart = loader.charts[loader.charts.length - 1];
+      const series = chart.series[0] as unknown as Highcharts.Series;
+      expect(series.color).toEqual('hotpink');
+      done();
+    });
   });
 });
