60b79708dcde36ef4c2b17bf6dbc5fd2167229f1
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-11-29 23:54:33 +0100

    correct demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import ExportingModule from 'highcharts/modules/exporting';
-import SunsetTheme from 'highcharts/themes/sunset.js';
-import * as Highcharts from 'highcharts';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { StockChartComponent } from './stock-chart/stock-chart.component';
 import { MapChartComponent } from './map-chart/map-chart.component';
 import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
 import { LazyLoadingChartComponent } from './lazy-loading-chart/lazy-loading-chart.component';
 
-// The modules will work for all charts.
-ExportingModule(Highcharts);
-SunsetTheme(Highcharts);
-
-Highcharts.setOptions({
-  title: {
-    style: {
-      color: 'tomato'
-    }
-  },
-  legend: {
-    enabled: false
-  }
-});
 
 @Component({
   selector: 'app-root',
@@ -30,7 +13,4 @@ Highcharts.setOptions({
   imports: [LineChartComponent, StockChartComponent, MapChartComponent, GanttChartComponent, LazyLoadingChartComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-export class AppComponent {
-
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
+import ExportingModule from 'highcharts/modules/exporting';
+import SunsetTheme from 'highcharts/themes/sunset.js';
+import { provideHighCharts } from '../highcharts-angular/src/public_api';
 import { environment } from './environments/environment';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
@@ -11,7 +14,21 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideHighCharts({
+      options: {
+        title: {
+          style: {
+            color: 'tomato'
+          }
+        },
+        legend: {
+          enabled: false
+        }
+      },
+      // The modules will work for all charts.
+      modules: [ExportingModule, SunsetTheme]
+    })
   ]
 })
   .catch(err => console.log(err));
