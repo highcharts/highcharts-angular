04b661ca4f308db9748c7cafe6500d1457903e9e
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-12-27 10:56:04 +0100

    fix: clean up highcharts-chart.directive.ts by removing unused imports and simplifying chart creation logic

--- highcharts-angular/src/lib/highcharts-chart.directive.ts
+++ highcharts-angular/src/lib/highcharts-chart.directive.ts
@@ -1,21 +1,8 @@
-import {
-  afterRenderEffect,
-  computed,
-  DestroyRef,
-  Directive,
-  effect,
-  ElementRef,
-  inject,
-  input,
-  model,
-  output,
-  PLATFORM_ID,
-  untracked,
-} from '@angular/core';
+import { DestroyRef, Directive, effect, ElementRef, inject, input, model, output, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 import { HighchartsChartService } from './highcharts-chart.service';
 import { HIGHCHARTS_CONFIG, HIGHCHARTS_TIMEOUT } from './highcharts-chart.token';
-import { ChartConstructorType, ConstructorChart } from './types';
+import { ChartConstructorType } from './types';
 import type Highcharts from 'highcharts/esm/highcharts';
 
 @Directive({
@@ -28,6 +15,7 @@ export class HighchartsChartDirective {
   public readonly constructorType = input<ChartConstructorType>('chart');
 
   /**
+   * @deprecated Will be removed in a future release.
    * When enabled, Updates `series`, `xAxis`, `yAxis`, and `annotations` to match new options.
    * Items are added/removed as needed. Series with `id`s are matched by `id`;
    * unmatched items are removed. Omitted `series` leaves existing ones unchanged.
@@ -40,6 +28,7 @@ export class HighchartsChartDirective {
   public readonly options = input.required<Highcharts.Options>();
 
   /**
+   * @deprecated Will be removed in a future release.
    * Whether to redraw the chart.
    * Check how update works in Highcharts
    * API doc here: https://api.highcharts.com/class-reference/Highcharts.Chart#update
@@ -64,57 +53,34 @@ export class HighchartsChartDirective {
 
   private readonly highchartsChartService = inject(HighchartsChartService);
 
-  private chartCreated = false;
-
-  private readonly constructorChart = computed<ConstructorChart | undefined>(() => {
-    const highCharts = this.highchartsChartService.highcharts();
-    if (highCharts) {
-      return (highCharts as any)[this.constructorType()];
-    }
-    return undefined;
-  });
+  private _chartInstance: Highcharts.Chart | undefined;
 
   private delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  // Create the chart as soon as we can
-  private readonly chart = computed(async () => {
-    await this.delay(this.relativeConfig?.timeout ?? this.timeout ?? 500);
-    return this.constructorChart()?.(
-      this.el.nativeElement,
-      // Use untracked, so we don't re-create new chart everytime options change
-      untracked(() => this.options()),
-      // Use Highcharts callback to emit chart instance, so it is available as early
-      // as possible. So that Angular is already aware of the instance if Highcharts raise
-      // events during the initialization that happens before coming back to Angular
-      createdChart => this.chartInstance.emit(createdChart),
-    );
-  });
-
   private keepChartUpToDate(): void {
     effect(async () => {
-      // Wait for the chart to be created
-      this.update();
-
-      const chart = await this.chart();
-
-      if (!this.chartCreated) {
-        if (chart) {
-          this.chartCreated = true;
-        }
-      } else {
-        chart?.update(this.options(), true, this.oneToOne());
+      await this.delay(this.relativeConfig?.timeout ?? this.timeout ?? 500);
+      const highCharts = this.highchartsChartService.highcharts();
+      const options = this.options();
+      const constructorType = this.constructorType();
+      if (!highCharts) return;
+      if (this._chartInstance) {
+        this._chartInstance.destroy();
+        this._chartInstance = undefined;
       }
+      const instance: Highcharts.Chart = (highCharts as any)[constructorType](
+        this.el.nativeElement,
+        // Use untracked, so we don't re-create new chart everytime options change
+        options,
+        // Use Highcharts callback to emit chart instance, so it is available as early
+        // as possible. So that Angular is already aware of the instance if Highcharts raise
+        // events during the initialization that happens before coming back to Angular
+        (createdChart: Highcharts.Chart) => this.chartInstance.emit(createdChart),
+      );
+      if (!this._chartInstance) this._chartInstance = instance;
     });
-  }
-
-  private async destroyChart(): Promise<void> {
-    const chart = await this.chart();
-    if (chart) {
-      // #56
-      chart.destroy();
-    }
   }
 
   public constructor() {
@@ -124,12 +90,7 @@ export class HighchartsChartDirective {
     }
     // make sure to load global config + modules on demand
     this.highchartsChartService.load(this.relativeConfig);
-    this.destroyRef.onDestroy(() => this.destroyChart()); // #44
-    afterRenderEffect(() => {
-      if (this.update()) {
-        this.update.set(false); // clear the flag after update
-      }
-    });
+    this.destroyRef.onDestroy(() => this._chartInstance?.destroy()); // #44
 
     // Keep the chart up to date whenever options change or the update special input is set to true
     this.keepChartUpToDate();
