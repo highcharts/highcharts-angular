b311c6f41c3db43085090ef5f3cb1c361e132266
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-12-30 12:38:09 +0100

    fix: adjust timeout configuration in test setup for highcharts-chart component

--- highcharts-angular/src/lib/highcharts-chart.component.spec.ts
+++ highcharts-angular/src/lib/highcharts-chart.component.spec.ts
@@ -25,7 +25,7 @@ describe('TestComponent / HighchartsChartService (load module)', () => {
     //    - provideHighcharts(): should wire up HIGHCHARTS_LOADER, etc., so the service can load core Highcharts.
     await TestBed.configureTestingModule({
       imports: [TestComponent],
-      providers: [provideHighcharts()],
+      providers: [provideHighcharts({ timeout: 1 })],
     }).compileComponents();
   });
 
@@ -78,7 +78,6 @@ describe('TestComponent / HighchartsChartService (load module)', () => {
         expect(hc.seriesTypes?.arearange).toBeDefined();
         expect(hc.seriesTypes?.dumbbell).toBeDefined();
       },
-      timeout: 2000,
     },
     {
       title: 'pattern-fill → adds SVGRenderer.addPattern',
