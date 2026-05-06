7797b2ce22a70eb85668d83f001d0d47851cd578
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2026-05-06 11:34:54 +0200

    Revert main.server.ts change.

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,9 @@
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { ApplicationRef } from '@angular/core';
 
-const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> =>
-  bootstrapApplication(AppComponent, config, context);
+const bootstrap = (): Promise<ApplicationRef> => bootstrapApplication(AppComponent, config);
+
 
 export default bootstrap;
